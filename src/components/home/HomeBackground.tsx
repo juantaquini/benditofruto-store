44ec5eb4625492a8cf41900ffb302c8b5a32ffde
juantaquini <taquinibautista@gmail.com>
2026-03-28 18:29:10 -0300

    Update HomeBackground.tsx

--- src/components/home/HomeBackground.tsx
+++ src/components/home/HomeBackground.tsx
@@ -4,7 +4,15 @@ import Image from "next/image";
 
 export default function HomeBackground() {
   return (
-    <div className="fixed inset-0 -z-10 bg-white">
+    <div
+      className={[
+        "fixed inset-0 -z-10 bg-white",
+        /* Móvil: no cubrir header fijo (py-4 + h-16 + py-4) ni la banda del footer; safe areas para notch/home indicator */
+        "max-md:top-[max(6rem,calc(env(safe-area-inset-top,0px)+5rem))]",
+        "max-md:bottom-[calc(9rem+env(safe-area-inset-bottom,0px))]",
+        "md:top-0 md:bottom-0",
+      ].join(" ")}
+    >
       <Image
         src="/images/Bendito_Fruto_045mobile.JPG"
         alt="Background"
